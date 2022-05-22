167bbb89266528b850104ec18e1c484cbcdfb0ac
Nelson <franciscosilvanelson@gmail.com>
2022-05-22 00:38:13 -0300

    feat(characters): add card component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,25 @@
 import React from 'react';
 
+import {Card} from '~/components/data-display/card';
+import GlobalStyle from '~/components/general/global-style';
+
 function App() {
 	return (
-		<div />
+		<>
+			<GlobalStyle />
+
+			<Card
+				width={240}
+				cover={
+					<img
+						alt='example'
+						src='https://rickandmortyapi.com/api/character/avatar/1.jpeg'
+					/>
+				}
+			>
+				<Card.Meta title='Rick Sanchez' description='Alive, Human' />
+			</Card>
+		</>
 	);
 }
 
---
+++ src/components/general/global-style/index.ts
@@ -0,0 +1,10 @@
+import {createGlobalStyle} from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background-color: #242221;
+		font-family: 'Roboto';
+  }
+`;
+
+export default GlobalStyle;
